93ef3cf727bb28d3a363fde08e67607f4a2b3bba
Alexandr Kiselev <kis.alex97@gmail.com>
2022-01-22 02:37:05 +0300

    added average.py

---
+++ lesson4/average.py
@@ -0,0 +1,6 @@
+first_number = 2308
+second_number = 13
+average = (first_number + second_number) / 2
+print(f"Первое число: {first_number}")
+print(f"Второе число: {second_number}")
+print(f"Среднее: {average}")
