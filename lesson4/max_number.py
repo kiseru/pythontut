e2edb5b92f6ea3e7a918e7928a0f2d856777115c
Alexandr Kiselev <kis.alex97@gmail.com>
2022-01-22 02:43:40 +0300

    added max_number.py

---
+++ lesson4/max_number.py
@@ -0,0 +1,8 @@
+first_number = 172.69
+second_number = 21.514
+print(first_number)
+print(second_number)
+if (first_number > second_number):
+    print(f"Наибольшее число: {first_number}")
+else:
+    print(f"Наибольшее число: {second_number}")
