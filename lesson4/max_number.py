dd392c3e3c2dbc056fe7cebcb9c2c8ce5971a969
Alexandr Kiselev <kis.alex97@gmail.com>
2022-01-22 02:44:48 +0300

    removed redundant parentheses in max_number.py

--- lesson4/max_number.py
+++ lesson4/max_number.py
@@ -2,7 +2,7 @@ first_number = 172.69
 second_number = 21.514
 print(first_number)
 print(second_number)
-if (first_number > second_number):
+if first_number > second_number:
     print(f"Наибольшее число: {first_number}")
 else:
     print(f"Наибольшее число: {second_number}")
