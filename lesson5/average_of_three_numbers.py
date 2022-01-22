f5a1b10e647dfbb980327560a6bb7c347a16a98a
Alexandr Kiselev <kis.alex97@gmail.com>
2022-01-22 10:38:55 +0300

    added average_of_three_numbers.py

---
+++ lesson5/average_of_three_numbers.py
@@ -0,0 +1,5 @@
+number1 = float(input("Введите число 1: "))
+number2 = float(input("Введите число 2: "))
+number3 = float(input("Введите число 3: "))
+average = (number1 + number2 + number3) / 3
+print(f"Среднее арифметическое: {average}")
