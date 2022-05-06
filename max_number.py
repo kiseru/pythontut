7c9cd22556f8933927f586bdc6a71efe3625076d
Alexandr Kiselev <kis.alex97@gmail.com>
2022-05-06 21:57:30 +0300

    fixed max_number.py by added ternary operator

--- max_number.py
+++ max_number.py
@@ -2,7 +2,5 @@ first_number = 172.69
 second_number = 21.514
 print(first_number)
 print(second_number)
-if first_number > second_number:
-    print(f"Наибольшее число: {first_number}")
-else:
-    print(f"Наибольшее число: {second_number}")
+bigger_number = first_number if first_number > second_number else second_number
+print(f'Наибольшее число: {bigger_number}')
